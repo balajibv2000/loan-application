bb1b6bdf8208120bfe3d256d6a820db0c20709fd
Balaji Bv <balajibv@Balaji.local>
2022-06-07 23:07:43 +0530

    basic UI

---
+++ src/container/Form.js
@@ -0,0 +1,20 @@
+const Form = () => {
+  return (
+    <div className="form-container">
+        <h3>Loan Application</h3>
+        <ul class="nav nav-tabs">
+            <li class="nav-item nav-link">
+                Personal Details
+            </li>
+            <li class="nav-item nav-link active">
+                Bussiness Details
+            </li>
+            <li class="nav-item nav-link">
+                Bussiness Details
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default Form
